cbd9607f9ef81b2dc5ae91916c4c2f641f6c7a7c
Yuchen-L <yl4250@columbia.edu>
2020-09-20 01:09:24 -0400

    second try of submission

--- proj1a/ArrayDeque.java
+++ proj1a/ArrayDeque.java
@@ -1,4 +1,103 @@
-package PACKAGE_NAME;
+public class ArrayDeque<T> {
+    public T[] items;
+    private int Size;
 
-public class ArrayDeque {
+    public ArrayDeque() {
+        items = (T[]) new Object [8];
+        Size = 0;
+    }
+
+//    public ArrayDeque(T[] i) {
+//        items = (T[]) new Object[8];
+//        System.arraycopy(i, 0, items, 0, i.length);
+//        Size += i.length;
+//    }
+
+    public void addFirst(T i) {
+        if(Size == items.length) {
+            resize(Size * 2);
+        }
+        System.arraycopy(items, 0, items, 1 , Size );
+        items[0] = i;
+        Size += 1;
+    }
+
+    public void addLast(T i) {
+        if(Size == items.length) {
+            resize(Size * 2);
+        }
+        items[Size] = i;
+        Size += 1;
+    }
+
+    public boolean isEmpty() {
+        if(Size == 0) {
+            return true;
+        }
+        return false;
+    }
+
+    public int size() {
+        return Size;
+    }
+
+    public void printDeque() {
+        for(int i =0 ; i < Size; i++) {
+            System.out.print(items[i] + " ");
+        }
+        System.out.println();
+    }
+
+    public T removeFirst() {
+        if(Size == 0) {
+            return null;
+        }
+        if(Size > 16 && ((float)Size / items.length < 0.25)) {
+            resize(Size / 2);
+        }
+        T tmp = items[0];
+        Size -= 1;
+        System.arraycopy(items, 1 , items, 0, Size);
+        return tmp;
+    }
+
+    public T removeLast() {
+        if(Size == 0) {
+            return null;
+        }
+        if(Size > 16 && ((float)Size / items.length < 0.25)) {
+            resize(Size / 2);
+        }
+        T tmp = items[Size-1];
+        items[Size-1] = null;
+        Size -= 1;
+        return tmp;
+
+    }
+
+    public T get(int Index) {
+        return items[Index];
+    }
+
+    public ArrayDeque(ArrayDeque other) {
+        System.arraycopy(other, 0, items, 0, other.Size);
+    }
+
+    public void resize(int Index) {
+        T[] tmp = (T[]) new Object[Index];
+        System.arraycopy(items, 0 , tmp , 0, Size);
+        items = tmp;
+    }
+
+
+//    public static void main(String[] args) {
+//        ArrayDeque<Integer> ad1 = new ArrayDeque<>();
+//        ad1.addFirst(100);
+//        int tr = ad1.removeFirst();
+//
+//        System.out.print(tr);
+//        System.out.println((float)1/4);
+//
+//    }
+
 }
--- proj1a/LinkedListDeque.java
+++ proj1a/LinkedListDeque.java
@@ -1,4 +1,132 @@
-package PACKAGE_NAME;
+public class LinkedListDeque<T> {
+    private Node sentinel;
+    private int Size;
 
-public class LinkedListDeque {
+    private class Node {
+        public Node prev;
+        public T item;
+        public Node next;
+
+        public Node (Node p, T i, Node n) {
+            prev = p;
+            item = i;
+            next = n;
+        }
+    }
+
+    public LinkedListDeque() {
+        sentinel = new Node(null, null ,null);
+        sentinel.prev = sentinel;
+        sentinel.next = sentinel;
+        Size = 0;
+    }
+
+    public LinkedListDeque(T i) {
+        sentinel = new Node(null, null , null);
+        sentinel.prev = sentinel.next = new Node(sentinel, i, sentinel);
+        Size = 1;
+    }
+
+
+
+    public void addFirst(T item) {
+        Node tmp = new Node(sentinel, item, sentinel.next);
+        sentinel.next.prev = tmp;
+        sentinel.next = tmp;
+        Size += 1;
+    }
+
+    public void addLast(T item) {
+        Node tmp = new Node(sentinel.prev, item, sentinel);
+        sentinel.prev.next = tmp;
+        sentinel.prev = tmp;
+        Size += 1;
+    }
+
+    public boolean isEmpty() {
+        if (Size == 0) {
+            return true;
+        }
+        return false;
+    }
+
+    public int size() {
+        return Size;
+    }
+
+    public void printDeque() {
+        for(Node i = sentinel.next; i != sentinel ; i = i.next ) {
+            System.out.print(i.item + " ");
+        }
+        System.out.println();
+    }
+
+    public T removeFirst(){
+        if(Size == 0) {
+            return null;
+        }
+        Node tmp = sentinel.next;
+        sentinel.next = sentinel.next.next;
+        sentinel.next.prev = sentinel;
+        Size -= 1;
+        return tmp.item;
+    }
+
+    public T removeLast() {
+        if(Size == 0) {
+            return null;
+        }
+        Node tmp = sentinel.prev;
+        sentinel.prev = sentinel.prev.prev;
+        sentinel.prev.next = sentinel;
+        Size -= 1;
+        return tmp.item;
+    }
+
+    public T get(int index) {
+        if(index > Size + 1) {
+            return null;
+        }
+        Node tmp = sentinel.next;
+        while(index > 0) {
+            tmp = tmp.next;
+            index -= 1;
+        }
+        return tmp.item;
+    }
+
+    public T getRecursive(int index){
+        return getRecursiveHelper(sentinel, index);
+    }
+
+    private T getRecursiveHelper(Node n, int index) {
+        Node tmp = n.next;
+        if(index == 0) {
+            return tmp.item;
+        }
+        return getRecursiveHelper(n.next, index-1 );
+    }
+
+    public LinkedListDeque(LinkedListDeque other) {
+        for(int i = other.Size; i > 0 ; i--) {
+            T item_o = (T) other.get(i);
+            this.addFirst(item_o);
+        }
+    }
+
+
+//    public static void main(String[] args) {
+////        LinkedListDeque<Integer> L= new LinkedListDeque<>(2);
+//        LinkedListDeque<Integer> EL= new LinkedListDeque<>();
+//        EL.addFirst(1);
+//        int a = 1;
+//        System.out.print(a + " ");
+//        if(3 > a + 1) {
+//            System.out.print("a");
+//        }
+//            System.out.println();
+//
+////        System.out.println(EL.Size);
+////        System.out.println(L.sentinel.next.next.item);
+//    }
 }
